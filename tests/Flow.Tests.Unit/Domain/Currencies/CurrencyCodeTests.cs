d8262e4e8805d2652e0b2f56a8b343b210969d5a
Vladislav.Karpovich <vladislav.karpovich.work@gmail.com>
2024-09-16 10:44:14 +0300

    Testing - Add tests for `Currencies`

---
+++ tests/Flow.Tests.Unit/Domain/Currencies/CurrencyCodeTests.cs
@@ -0,0 +1,55 @@
+﻿using Flow.Domain.Abstractions;
+using Flow.Domain.Currencies;
+using FluentAssertions;
+
+namespace Flow.Tests.Unit.Domain.Currencies;
+
+public sealed class CurrencyCodeTests
+{
+    [Fact]
+    public void Return_NullValueError_When_CurrencyCode_Is_Null()
+    {
+        // Arrange
+
+        // Act
+        var result = CurrencyCode.Create(null);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(Error.NullValueError);
+    }
+
+    [Theory]
+    [InlineData("EUR O")]
+    [InlineData("EU")]
+    [InlineData("EURO")]
+    [InlineData("E R")]
+    public void Return_ExactLengthError_When_CurrencyCode_Length_Not_ThreeLetters(string currencyCode)
+    {
+        // Arrange
+
+        // Act
+        var result = CurrencyCode.Create(currencyCode);
+
+        // Assert
+        result.Error.Should().Be(Error.ExactLengthError(3));
+    }
+
+    [Theory]
+    [InlineData("EUR", "EUR")]
+    [InlineData("usd ", "USD")]
+    [InlineData(" BYn", "BYN")]
+    [InlineData("R U B", "RUB")]
+    public void Return_CreatedCurrencyCode_When_CurrencyCode_Valid(string value, string expected)
+    {
+        // Arrange
+
+        // Act
+        var result = CurrencyCode.Create(value);
+
+        // Assert
+        var currencyCode = result.Value;
+        currencyCode.Value.Should().Be(expected);
+    }
+}
---
+++ tests/Flow.Tests.Unit/Domain/Currencies/CurrencyTests.cs
@@ -0,0 +1,28 @@
+﻿using Flow.Domain.Currencies;
+using FluentAssertions;
+
+namespace Flow.Tests.Unit.Domain.Currencies;
+
+public sealed class CurrencyTests
+{
+    [Fact]
+    public void Return_CreatedCurrency_When_IsValid()
+    {
+        // Arrange
+        var createdAt = DateTime.Now;
+        var currencyCode = CurrencyCode.Create("EUR");
+
+        // Act
+        var result = Currency.Create(currencyCode.Value, createdAt);
+
+        // Assert
+        var currency = result.Value;
+
+        currency.Id.Should().NotBeNull();
+        currency.Code.Should().Be(currencyCode.Value);
+        currency.CreatedAt.Should().Be(createdAt);
+        currency.UpdatedAt.Should().BeNull();
+        currency.IsDeactivated.Should().BeFalse();
+        currency.DeactivatedAt.Should().BeNull();
+    }
+}
